b6c284b01e78684f492b872db317c4e143dd5568
Paolo Rapuano <bettercallpaul07@gmail.com>
2023-03-08 16:08:10 +0100

    Done

---
+++ resources/views/HelloWorld.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Laravel</title>
+
+    <!-- Bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Hello Laravel</h1>
+
+
+    <ul>
+        <li><a href="/Contacts">Contatti</a></li>
+        <li><a href="/Registration">Registrazione</a></li>
+    </ul>
+</body>
+
+</html>
+
+
+<style>
+    body {
+        font-family: 'Nunito', sans-serif;
+        background-color: #1A202C;
+        color: white;
+    }
+</style>
